ea98a40262c45191fce2d1bd4b98d425d6e3fddd
Robert <rbbenett0614@gmail.com>
2021-01-05 17:36:35 +0000

    Add table for teachers and assistance-requests

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,9 +1,9 @@
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(225) NOT NULL,
-  is_active BOOLEAN DEFAULT TRUE
+  is_active BOOLEAN DEFAULT TRUE,
   start_date DATE,
-  end_date DATE,
+  end_date DATE
 );
 
 CREATE TABLE assistance_requests (
